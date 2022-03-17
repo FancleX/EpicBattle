4a81864deec00484b1f09d4a7c75b69f01fb1274
chen <fancle666@gmail.com>
2022-03-17 16:49:06 -0400

    update

--- src/music/Music.java
+++ src/music/Music.java
@@ -4,8 +4,11 @@ import jaco.mp3.player.MP3Player;
 import java.io.File;
 
 public class Music {
+    // add music to GrameFrame
     public Music() {
         String path = System.getProperty("user.dir") + "/src/music/music.mp3";
-        new MP3Player(new File(path)).play();;
+        MP3Player player = new MP3Player(new File(path));
+        player.setRepeat(true);
+        player.play();
     }
 }
